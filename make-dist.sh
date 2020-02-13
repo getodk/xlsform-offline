43fc991e3fe1b6484108b1b13d0694f65f2c9083
shourj@uw.edu <Pl@zmatv1>
2020-02-13 14:16:28 -0800

    removed version info

--- make-dist.sh
+++ make-dist.sh
@@ -6,12 +6,10 @@ README='pkg/Read me.txt'
 MAC_BINARY='dist/mac/ODK XLSForm Offline.app'
 MAC_README='dist/mac/Read me.txt'
 
-VERSION=$(grep VERSION src/main.py | sed -e "s|.*'\(.*\)'|\1|")
-
 if [[ -e "$MAC_BINARY" ]]; then
 	echo 'Mac version exists';
 	fold -w 80 -s "$README" > "$MAC_README";
 	cd dist/mac;
-	zip -r9 "ODK-XLSForm-Offline-macOS-$VERSION.zip" . -x "*.DS_Store"
+	zip -r9 "ODK-XLSForm-Offline-macOS.zip" . -x "*.DS_Store"
 	cd ../..;
 fi
