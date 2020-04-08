78af92984790800510212db3056f762723e1f420
Mitchell Estberg <estbergm@uw.edu>
2020-04-07 23:04:17 -0700

    Update Travis
    Co-authored-by: Pieter Benjamin <pieter0benjamin@gmail.com>
    Co-authored-by: Shourya Jain <shourj@uw.edu>

--- make-dist.sh
+++ make-dist.sh
@@ -1,15 +1,46 @@
+  
 #!/bin/bash
 set -e
 
+VERSION=$(git describe --tags --dirty --always)
+
 README='pkg/Read me.txt'
 
-MAC_BINARY='dist/mac/ODK XLSForm Offline.app'
-MAC_README='dist/mac/Read me.txt'
+CWD=$(pwd)
+
+MAC_BINARY="dist/mac/ODK-XLSForm-Offline.app"
+MAC_README="dist/mac/Read me.txt"
+
+# Travis workspaces is weird with windows
+WIN_DIR="C:${CWD}/dist/win"
+WIN_BINARY="${WIN_DIR}/ODK-XLSForm-Offline.exe"
+WIN_README="${WIN_DIR}/Read me.txt"
 
 if [[ -e "$MAC_BINARY" ]]; then
 	echo 'Mac version exists';
 	fold -w 80 -s "$README" > "$MAC_README";
 	cd dist/mac;
-	zip -r9 "ODK-XLSForm-Offline-macOS.zip" . -x "*.DS_Store"
-	cd ../..;
+	mv "ODK-XLSForm-Offline.app" "ODK XLSForm Offline.app" 
+	# Add the binary version in the zip name
+	zip -r9 "ODK-XLSForm-Offline-macOS-${VERSION}.zip" . -x "*.DS_Store"
+	echo "ODK-XLSForm-Offline-macOS-${VERSION}.zip"
+	cd $CWD;
+fi
+
+if [[ -e "$WIN_BINARY" ]]; then
+	if ! [[ -d dist ]]; then
+		mkdir dist
+	fi
+	mkdir dist/win
+	echo 'Windows version exists';
+	fold -w 80 -s "$README" > "$WIN_README";
+	mac2unix "$WIN_README" && unix2dos "$WIN_README";
+	cd $WIN_DIR;
+	ls
+	mv "ODK-XLSForm-Offline.exe" "ODK XLSForm Offline.exe"
+	zip -r9 "ODK-XLSForm-Offline-Windows.zip" . -x "*.DS_Store"
+	# Add the binary version in the zip name
+	mv "ODK-XLSForm-Offline-Windows.zip" "$CWD/dist/win/ODK-XLSForm-Offline-Windows-${VERSION}.zip"
+	echo "$CWD/dist/win/ODK-XLSForm-Offline-Windows-${VERSION}.zip"
+	cd $CWD;
 fi
--- make-win.bat
+++ make-win.bat
@@ -1,4 +1,5 @@
+pip install PyInstaller wxpython pyxform
 rd /s /q build\xlsform-offline-win 
 rd /s /q dist\win
 del /s /q *.pyc
-pyinstaller pkg\xlsform-offline-win.spec --distpath dist\win --onefile --windowed --noconfirm --clean
+python -m PyInstaller pkg\xlsform-offline-win.spec --distpath dist\win --onefile --windowed --noconfirm --clean
